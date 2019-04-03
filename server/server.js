464019d987a1774d9351f9b3a8df65de1477db7d
Bioney Dominguez <Bioney@MacBook-Pro-de-Ingrid.local>
2019-04-02 22:08:42 -0600

    alcance 1.4

--- server/server.js
+++ server/server.js
@@ -1,5 +1,39 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
+const mongoose = require('mongoose')
+const { User } = require('./models/user')
+
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true}, (error, res) => {
+    if (error) {
+        throw error
+    }
+    else {
+        console.log("Estoy escuchando Base de datos")
+    }
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.post('/api/users/register', (req, res)=> {
+   const user = new User(req.body)
+   user.save((err, doc) => {
+    if(err) return res.json({success: false, err} )
+    res.status(200).json({
+        success: true,
+        userdata: doc
+    })
+   })
+    
+   
+})
+
+
+
 const port = process.env.PORT || 3002
 
 app.listen(port, () => {
